766dbb8878c2df8d3f7eb7dcb42844ce12a7df30
Ramani Gopi <gopiramani99257@gmail.com>
2025-04-18 08:01:13 +0000

    machine

---
+++ machine/script.js
@@ -0,0 +1,8 @@
+// var menuButton = document.getElementById('hamburger');
+// var navBar = document.getElementsByClassName('nav-bar');
+// console.log(navBar)
+
+// menuButton.addEventListener( 'click',function(e){
+    
+//      navBar[0].style.display = 'block';
+// } );
